90089f97fc19d5264a88901416606c856f3aabf6
milicolazo <milirita.colazovinovo@gmail.com>
2020-10-12 20:14:47 -0300

    Correct testing with np.testing.array_equal fix #2

--- test_pyedra.py
+++ test_pyedra.py
@@ -1,11 +1,17 @@
 import pyedra
 import pandas as pd
+import pytest
+import numpy as np
+
+@pytest.fixture(scope="session")
+def dataset_3():
+    return pd.read_csv('test_data/prueba3.csv')
 
 
-df=pd.read_csv('prueba3.csv')
-def test_HG():
-    result=HG_fit(df)
-    for nro in df.nro
-        assert result.Asteroid==nro
+def test_HG_fit(dataset_3):
+    df = dataset_3
+    noob = df.drop_duplicates(subset="nro", keep="first", inplace=False)
+    result = pyedra.HG_fit(df)
+    np.testing.assert_array_equal(noob.nro, result.Asteroid)
     
   
