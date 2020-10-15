0b4b5d32f23b07605257823116c76fa2b92e654e
milicolazo <milirita.colazovinovo@gmail.com>
2020-10-15 11:44:00 -0300

    change variable names in test_pyedra.py

--- test_pyedra.py
+++ test_pyedra.py
@@ -8,66 +8,66 @@ import pytest
 
 
 @pytest.fixture(scope="session")
-def ground():
-    return pd.read_csv("test_data/testdata_ground.csv")
+def data():
+    return pd.read_csv("test_data/testdata_carbognani2019.csv")
 
 
 @pytest.fixture(scope="session")
-def carbognani():
-    return pd.read_csv("test_data/testdata_carbognani.csv")
+def carbognani2019():
+    return pd.read_csv("test_data/carbognani2019.csv")
 
 
-def test_HG_fit(ground, carbognani):
-    noob = ground.drop_duplicates(subset="nro", keep="first", inplace=False)
-    result = pyedra.HG_fit(ground)
+def test_HG_fit(data, carbognani2019):
+    noob = data.drop_duplicates(subset="nro", keep="first", inplace=False)
+    result = pyedra.HG_fit(data)
 
     np.testing.assert_array_equal(noob.nro, result.Asteroid)
 
     for idx, error in enumerate(result.error_H):
         np.testing.assert_allclose(
-            carbognani.H[idx], result.H[idx], atol=error
+            carbognani2019.H[idx], result.H[idx], atol=error
         )
     for idx, error in enumerate(result.error_G):
         np.testing.assert_allclose(
-            carbognani.G[idx], result.G[idx], atol=error
+            carbognani2019.G[idx], result.G[idx], atol=error
         )
 
 
-def test_HG1G2_fit(ground, carbognani):
-    noob = ground.drop_duplicates(subset="nro", keep="first", inplace=False)
-    result = pyedra.HG1G2_fit(ground)
+def test_HG1G2_fit(data, carbognani2019):
+    noob = data.drop_duplicates(subset="nro", keep="first", inplace=False)
+    result = pyedra.HG1G2_fit(data)
 
     np.testing.assert_array_equal(noob.nro, result.Asteroid)
 
     for idx, error in enumerate(result.error_H12):
         np.testing.assert_allclose(
-            carbognani.H12[idx], result.H12[idx], atol=error
+            carbognani2019.H12[idx], result.H12[idx], atol=error
         )
     for idx, error in enumerate(result.error_G1):
         np.testing.assert_allclose(
-            carbognani.G1[idx], result.G1[idx], atol=error
+            carbognani2019.G1[idx], result.G1[idx], atol=error
         )
     for idx, error in enumerate(result.error_G2):
         np.testing.assert_allclose(
-            carbognani.G2[idx], result.G2[idx], atol=error
+            carbognani2019.G2[idx], result.G2[idx], atol=error
         )
 
 
-def test_Shev_fit(ground, carbognani):
-    noob = ground.drop_duplicates(subset="nro", keep="first", inplace=False)
-    result = pyedra.Shev_fit(ground)
+def test_Shev_fit(data, carbognani2019):
+    noob = data.drop_duplicates(subset="nro", keep="first", inplace=False)
+    result = pyedra.Shev_fit(data)
 
     np.testing.assert_array_equal(noob.nro, result.Asteroid)
 
     for idx, error in enumerate(result.error_V_lin):
         np.testing.assert_allclose(
-            carbognani.V_lin[idx], result.V_lin[idx], atol=error
+            carbognani2019.V_lin[idx], result.V_lin[idx], atol=error
         )
     for idx, error in enumerate(result.error_b):
         np.testing.assert_allclose(
-            carbognani.b[idx], result.b[idx], atol=error
+            carbognani2019.b[idx], result.b[idx], atol=error
         )
     for idx, error in enumerate(result.error_c):
         np.testing.assert_allclose(
-            carbognani.c[idx], result.c[idx], atol=error
+            carbognani2019.c[idx], result.c[idx], atol=error
         )
