39481a50e39e45623e51533e2e8a0f0ef22b1542
milicolazo <milirita.colazovinovo@gmail.com>
2020-10-19 12:02:40 -0300

    add header to all modules

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   Pyedra Project (https://github.com/milicolazo/Pyedra/).
+# Copyright (c) 2020, Milagros Colazo
+# License: MIT
+#   Full Text: https://github.com/milicolazo/Pyedra/blob/master/LICENSE
+
 from ez_setup import use_setuptools
 
 from setuptools import setup
