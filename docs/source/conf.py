450688a05d0f0be68827a3ed0f641f140dc24e8e
milicolazo <milirita.colazovinovo@gmail.com>
2020-10-29 09:13:07 -0300

    fix docs???

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,6 +52,8 @@ extensions = [
 ]
 
 napoleon_numpy_docstring = True
+poleon_use_param = False
+napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
